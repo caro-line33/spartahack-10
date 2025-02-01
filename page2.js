06056e349fb4997d8fc8ff62348e8bf242ca69aa
Caroline <huang244@msu.edu>
2025-02-01 18:03:40 -0500

    e
    hi

--- page2.js
+++ page2.js
@@ -1,21 +1,5 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const courseSearch = document.getElementById("courseSearch");
-    const selectedCourses = document.getElementById("selectedCourses");
-
-    courseSearch.addEventListener("keydown", (event) => {
-      if (event.key === "Enter") {
-        event.preventDefault();
-        const courseValue = courseSearch.value.trim();
-  
-        if (courseValue !== "") {
-          const courseElement = document.createElement("p");
-          courseElement.textContent = courseValue;
-  
-          selectedCourses.appendChild(courseElement);
-  
-          courseSearch.value = "";
-        }
-      }
-    });
-  });
+let GetJSONFile = async(file) => {
+    return await fetch(file).then(data => data.json())
+  }
+  GetJSONFile("class.json")
   
