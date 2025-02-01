f028593157411f1675fddbe34936f40370a396a1
Thom <fa.thoms1308@gmail.com>
2025-02-01 16:08:51 -0500

    updated main
    deleted some stuff lol

--- main.js
+++ main.js
@@ -7,23 +7,14 @@ class Course {
         this.credits = credits;
         this.prereqs = prereqs;
         this.semesters = semesters;
-
     }
 }
-
-const thisClass = new Course("ECE", "202", "Introduction to Circuits", "Fall");
 
 // removes all items in second list from first list
 function coursesNeeded(majorReq, alreadyTaken) {
     const takenSet = new Set(alreadyTaken);
     return majorReq.filter(course => !takenSet.has(course));
 }
-
-const majorArray = [1, 2, 3, 4]; 
-const alreadyTaken = [2];
-
-console.log(coursesNeeded(majorArray, alreadyTaken));
-
 
 const majorOptions = document.getElementById('majorOptions');
 majorOptions.addEventListener('change', storeMajor);
