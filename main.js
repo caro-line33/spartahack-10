93e981fcadb33036209698728cd03682f52b7b58
Caroline <huang244@msu.edu>
2025-02-01 13:48:30 -0500

    Update main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+function course(major, coursecode, coursename, semesters){
+    this.major = major;
+    this.coursecode = coursecode;
+    this.coursename = coursename;
+    this.semesters = semesters;
+}
+
+const thisclass = new course("ECE", "202", , "");
