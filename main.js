f3020fae84f73c5b9a6aa479d43c971983812e96
Caroline <huang244@msu.edu>
2025-02-01 15:30:30 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
-function course(major, coursecode, coursename, semesters){
-    this.major = major;
-    this.coursecode = coursecode;
-    this.coursename = coursename;
-    this.semesters = semesters;
+// Constructor function for Course objects.
+function Course(major, coursecode, coursename, semesters) {
+this.major = major;
+this.coursecode = coursecode;
+this.coursename = coursename;
+this.semesters = semesters;
 }
 
-const thisclass = new course("ECE", "202", , "");
 
-function courses_needed(majorreq, alreadytaken){
-    // assume both inputs arrays
-    const set1 = new Set(majorreq)
-    return majorreq.filter(course => !set2.has(course));
+const thisClass = new Course("ECE", "202", "Introduction to Circuits", "Fall");
+
+// removes all items in second list from first list
+function coursesNeeded(majorReq, alreadyTaken) {
+    const takenSet = new Set(alreadyTaken);
+    return majorReq.filter(course => !takenSet.has(course));
 }
 
-const majorarray = [1, 2, 3, 4];
-const alreadytaken = [2];
+const majorArray = [1, 2, 3, 4]; 
+const alreadyTaken = [2];
+
+console.log(coursesNeeded(majorArray, alreadyTaken));
