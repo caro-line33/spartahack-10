05c55bb4a615888c5f5bb0cc8bbc5e1824cbbe5c
Caroline <huang244@msu.edu>
2025-02-01 21:58:37 -0500

    hi

--- page3.js
+++ page3.js
@@ -1,21 +1,4 @@
-// lisa added this
-window.addEventListener('load', function () {
-  document.body.classList.add('loaded');
-});
 
-const confirm = document.getElementById('confirmButton');
-confirm.disabled = true;
-
-const buttons = document.querySelectorAll('.myButton');
-buttons.forEach(button => {
-  button.addEventListener('click', handleClick);
-});
-
-let difficulty = "";
-
-function handleClick(this){
-    console.log()
-}
 
 
 
