c75817a86fa95f2de9655c0ae3c0953efa29e19e
Caroline <huang244@msu.edu>
2025-02-01 22:30:11 -0500

    hi

--- page3.js
+++ page3.js
@@ -12,14 +12,9 @@ buttons.forEach(button => {
 });
 
 function saveDifficulty(event) {
-    // Convert the clicked button's value to an integer
     creditLimit = parseInt(event.target.value, 10);
     console.log(creditLimit);
-    
-    // Remove the 'selected' class from all difficulty buttons
     buttons.forEach(btn => btn.classList.remove('selected'));
-    
-    // Add the 'selected' class only to the clicked button
     event.target.classList.add('selected');
 }
 
@@ -27,7 +22,6 @@ confirmButton.addEventListener('click', nextPage);
 
 function nextPage(){
     if(creditLimit !== 0){
-        // Save the creditLimit to localStorage
         localStorage.setItem("creditLimit", creditLimit);
         window.location.href = "page4.html"; 
     }
